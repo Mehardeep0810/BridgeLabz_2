24e414e57dd3484c51a2ca50e4554ccab94bbb18
Mehardeep Kaur <mehardeepk.38@gmail.com>
2025-09-12 15:23:30 +0530

    FactorFinder.java

--- Arrays/level-1/FactorFinder.java
+++ Arrays/level-1/FactorFinder.java
@@ -6,7 +6,6 @@ public class FactorFinder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Step 1: Get user input
         System.out.print("Enter a positive integer to find its factors: ");
         int number = sc.nextInt();
 
@@ -16,12 +15,11 @@ public class FactorFinder {
             return;
         }
 
-        // Step 2: Initialize factor array and variables
         int maxFactor = 10;
         int[] factors = new int[maxFactor];
         int index = 0;
 
-        // Step 3: Find and store factors
+  
         for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
                 // Resize array if needed
@@ -37,7 +35,7 @@ public class FactorFinder {
             }
         }
 
-        // Step 4: Display factors
+    
         System.out.println("\nFactors of " + number + ":");
         for (int i = 0; i < index; i++) {
             System.out.print(factors[i] + " ");
@@ -45,4 +43,5 @@ public class FactorFinder {
 
         sc.close();
     }
+
 }
